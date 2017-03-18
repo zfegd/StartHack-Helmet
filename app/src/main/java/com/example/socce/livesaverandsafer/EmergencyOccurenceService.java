7ebf7584378a6648cb026d0691b40f1ff403b365
Ash <ashley.see16@imperial.ac.uk>
2017-03-18 14:26:52 +0000

    Updates

--- app/src/main/java/com/example/socce/livesaverandsafer/EmergencyOccurenceService.java
+++ app/src/main/java/com/example/socce/livesaverandsafer/EmergencyOccurenceService.java
@@ -2,10 +2,21 @@ package com.example.socce.livesaverandsafer;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.telephony.SmsManager;
+import android.util.Log;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.LatLng;
 
 public class EmergencyOccurenceService extends IntentService {
+
+    private GoogleApiClient apiClient;
 
     public EmergencyOccurenceService(String name) {
         super(name);
@@ -17,15 +28,34 @@ public class EmergencyOccurenceService extends IntentService {
 
 
         // DO below if there is an incident
+        if(apiClient==null){
+            apiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks((GoogleApiClient.ConnectionCallbacks) this)
+                    .addOnConnectionFailedListener((GoogleApiClient.OnConnectionFailedListener) this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+        LocationRequest locReq = new LocationRequest().setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        LocationListener listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
 
+            }
+        };
+        LocationServices.FusedLocationApi.requestLocationUpdates(apiClient,locReq,listener);
+        Location location = LocationServices.FusedLocationApi.getLastLocation(apiClient);
+        double longt = location.getLongitude();
+        double langt = location.getLatitude();
+        double alt = location.getAltitude();
+        String incident = "Incident at Long(" + longt + "), Lang(" + langt + ") & alt(" + alt + ")";
 
         SmsManager manager = SmsManager.getDefault();
-
-        manager.sendTextMessage("+972545877122",null,"Insert Message with GPS Here",null,null);
-        //Send SMS with GPS
+        manager.sendTextMessage("+972545877122",null,incident,null,null);
 
         Intent calling = new Intent(Intent.ACTION_CALL, Uri.parse("tel:+972545877122"));
         startActivity(calling);
 
         }
+
+
 }
--- app/src/main/java/com/example/socce/livesaverandsafer/MainActivity.java
+++ app/src/main/java/com/example/socce/livesaverandsafer/MainActivity.java
@@ -1,21 +1,38 @@
 package com.example.socce.livesaverandsafer;
 
 import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
-import android.net.Uri;
+import android.content.res.Resources;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.telephony.SmsManager;
 import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 
-    final int locRequestCode = 42;
-    final int phoneReqCode = 999;
-    final int smsRequestCode = 535;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    final int LOC_REQ_CODE = 42;
+    final int PHONE_REQ_CODE = 999;
+    final int SMS_REQ_CODE = 535;
+    final int BLUETOOTH_ENABLE_CODE = 423;
+    final String HELMET_NAME = "HELMET";
+    private Map<String, String> devicesFound =  new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,25 +42,40 @@ public class MainActivity extends AppCompatActivity {
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, locRequestCode);
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOC_REQ_CODE);
         }
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE)
                 != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.CALL_PHONE}, phoneReqCode);
+                    new String[]{Manifest.permission.CALL_PHONE}, PHONE_REQ_CODE);
         }
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS)
                 != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.SEND_SMS}, smsRequestCode);
+                    new String[]{Manifest.permission.SEND_SMS}, SMS_REQ_CODE);
         }
-
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(bluetoothAdapter==null){
+            Log.e("no bt","no bt");
+            System.exit(0);
+        }
+        if(! bluetoothAdapter.isEnabled()){
+            Intent enableBlueTooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBlueTooth,BLUETOOTH_ENABLE_CODE);
+        }
+        // Bluetooth is now enabled above
+        try {
+            bluetoothActions(bluetoothAdapter);
+        }
+        catch (Exception e){
+            Log.e("Not found","Not found");
+        }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults){
         switch (requestCode){
-            case locRequestCode: {
+            case LOC_REQ_CODE: {
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_DENIED){
                     Log.v("Denied","App needs permissions to work");
@@ -51,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
                 }
                 break;
             }
-            case phoneReqCode: {
+            case PHONE_REQ_CODE: {
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_DENIED) {
                     Log.v("Denied", "App needs permissions to work");
@@ -59,7 +91,7 @@ public class MainActivity extends AppCompatActivity {
                 }
                 break;
             }
-            case smsRequestCode: {
+            case SMS_REQ_CODE: {
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_DENIED) {
                     Log.v("Denied", "App needs permissions to work");
@@ -72,4 +104,73 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void bluetoothActions(BluetoothAdapter bluetoothAdapter) throws Resources.NotFoundException {
+        String address = queryKnown(bluetoothAdapter);
+        if(address.equals("")){
+            address = discover(bluetoothAdapter);
+        }
+        if(address.equals("")){
+            throw new Resources.NotFoundException("Device is not available!");
+        }
+        //Connect time
+    }
+
+    private String queryKnown(BluetoothAdapter bluetoothAdapter){
+        Set<BluetoothDevice> devices = bluetoothAdapter.getBondedDevices();
+        for(BluetoothDevice device : devices){
+            String devName = device.getName();
+            String devMacAdd = device.getAddress();
+            if(devName.equals(HELMET_NAME)){
+                return devMacAdd;
+            }
+        }
+        return "";
+    }
+
+    private String discover(final BluetoothAdapter bluetoothAdapter){
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(receiver,filter);
+        bluetoothAdapter.startDiscovery();
+//        try {
+//            wait(1300);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        if(devicesFound.containsKey(HELMET_NAME)){
+            return devicesFound.get(HELMET_NAME);
+        }
+        unregisterReceiver(receiver);
+        return "";
+    }
+
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+                devicesFound.put(deviceName,deviceHardwareAddress);
+            }
+        }
+    };
+
+    private void connect(BluetoothAdapter bluetoothAdapter){
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
 }
